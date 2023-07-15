f97e6eb9f5f7fce5fce8e5c6519be5b014c7b251
Dhiman Kumar Chakrabarty <78009274+dhman-computing@users.noreply.github.com>
2023-07-15 11:40:04 +0530

    Implemented writeToDatabase function

--- code/support.py
+++ code/support.py
@@ -1,7 +1,8 @@
 from pathlib import Path
+import sqlite3
 
 
-def getWordList(filePath):
+def getWordList(filePath : Path):
     '''
     Get a list of words from a .txt file. 
     Here fileName is an instance of Path object in pathlib to the file, 
@@ -49,4 +50,26 @@ def addToDict(words: list[str]):
 
 
     return wordDict
+
+def writeToDatabase(wordDict : dict, textName : str):
+    dbPath = Path("database/database.db")
+    con = sqlite3.connect(dbPath.as_posix())
+
+    crsr = con.cursor()
+
+    crsr.execute(f"CREATE TABLE IF NOT EXISTS {textName} (id INT, word TEXT, count INT)")
+
+    sortedKeysWD = sorted(wordDict.keys())
+    for index in sortedKeysWD:
+        indexDict = wordDict[index]
+        sortedKeysID = sorted(indexDict)
+
+        for word in sortedKeysID:
+            crsr.execute(f"INSERT INTO {textName} (word, count) VALUES (?, ?)", (word, indexDict[word]))
+
+    con.commit()
+    con.close()
+    print("Database Modified.")
+
+    return
 
